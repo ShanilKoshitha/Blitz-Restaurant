ae75b4044f75bb00b592a3214349c2fd84bfc044
Shanil Koshitha <shanilkoshitha@rocketmail.com>
2021-08-28 21:59:07 -0300

    service bus implementation start

--- Blitz.Web/Controllers/CartController.cs
+++ Blitz.Web/Controllers/CartController.cs
@@ -39,10 +39,29 @@ namespace Blitz.Web.Controllers
             }
             return View();
         }
-        [HttpGet]
+        
         public async Task<IActionResult> CheckOut()
         {
             return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckOut(CartDto cartDto)
+        {
+            try
+            {
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                var response = await _cartService.Checkout<ResponseDto>(cartDto.CartHeader, accessToken);
+                return RedirectToAction(nameof(Confirmation));
+            }catch(Exception e)
+            {
+                return View(cartDto);
+            }
+        }
+        
+        public async Task<IActionResult> Confirmation()
+        {
+            return View();
         }
 
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
